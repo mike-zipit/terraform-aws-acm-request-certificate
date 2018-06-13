f8c4d1308a993b5d90566b6faa11d7fd92630b24
Mike Crowe <32719656+mike-zipit@users.noreply.github.com>
2018-06-13 17:55:13 -0400

    Adding enabled flag

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "aws_acm_certificate" "default" {
+  count                     = "${var.enabled == "true" ? 1 : 0}"
   domain_name               = "${var.domain_name}"
   validation_method         = "${var.validation_method}"
   subject_alternative_names = ["${var.subject_alternative_names}"]
@@ -6,7 +7,7 @@ resource "aws_acm_certificate" "default" {
 }
 
 data "aws_route53_zone" "default" {
-  count        = "${var.proces_domain_validation_options == "true" && var.validation_method == "DNS" ? 1 : 0}"
+  count        = "${var.enabled == "true" && var.proces_domain_validation_options == "true" && var.validation_method == "DNS" ? 1 : 0}"
   name         = "${var.domain_name}."
   private_zone = false
 }
@@ -16,7 +17,7 @@ locals {
 }
 
 resource "aws_route53_record" "default" {
-  count   = "${var.proces_domain_validation_options == "true" && var.validation_method == "DNS" ? 1 : 0}"
+  count   = "${var.enabled == "true" && var.proces_domain_validation_options == "true" && var.validation_method == "DNS" ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.default.zone_id}"
   name    = "${local.domain_validation_options["resource_record_name"]}"
   type    = "${local.domain_validation_options["resource_record_type"]}"
